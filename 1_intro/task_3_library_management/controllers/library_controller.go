59b91dc736e5b96c151441b19315845c5f1283b7
Nathan M. Shiferaw <natms24@gmail.com>
2024-08-05 11:47:35 +0300

    Add intro:task 3 service

--- 1_intro/task_3_library_management/controllers/library_controller.go
+++ 1_intro/task_3_library_management/controllers/library_controller.go
@@ -3,14 +3,25 @@ package controllers
 import (
 	"bufio"
 	"fmt"
+	"library_management/models"
 	"library_management/services"
 	"os"
 	"strconv"
 	"strings"
 )
 
+var commands = map[string]string{
+	"Add book":             "add",
+	"Remove book":          "remove",
+	"Borrow book":          "borrow",
+	"Return book":          "return",
+	"List available books": "la",
+	"List borrowed books":  "lb",
+}
+
 func GetInputStr(reader *bufio.Reader, prompt string) string {
 	for {
+		fmt.Print(prompt)
 		input, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Print("\t\t**Error: ", err.Error())
@@ -23,6 +34,7 @@ func GetInputStr(reader *bufio.Reader, prompt string) string {
 
 func GetInputInt(reader *bufio.Reader, prompt string) int {
 	for {
+		fmt.Print(prompt)
 		input, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Print("\t\t**Error: ", err.Error())
@@ -39,37 +51,64 @@ func GetInputInt(reader *bufio.Reader, prompt string) int {
 	}
 }
 
+func LogBooks(books []models.Book, title string) {
+	fmt.Printf("\n%v\n\t%20v\t\t%20v\t\t%20v\n", title, "ID", "Title", "Author")
+	for _, book := range books {
+		fmt.Printf("\t%20v\t\t%20v\t\t%20v\n", book.ID, book.Title, book.Author)
+	}
+}
+
 func Run() {
 	reader := bufio.NewReader(os.Stdin)
 	active := true
-	fmt.Print("\nCommands:\n\tAdd book: 'add'\n\tRemove book: 'remove'\n\tBorrow book: 'borrow'\n\tReturn book: 'return'\n\tList available books: 'la'\n\tList borrowed books: 'lb'\n\tExit: 'q'\n")
-	for active {
-		cmd := GetInputStr(reader, "> ")
+	fmt.Print("\nCommands:\n")
+	for k, v := range commands {
+		fmt.Printf("\t- %-25v %v\n", k+":", v)
+	}
 
+	for active {
+		cmd := GetInputStr(reader, "\n> ")
 		switch cmd {
 		case "add":
 			title := GetInputStr(reader, "\tBook title: ")
 			author := GetInputStr(reader, "\tBook author: ")
 			services.HandleAddBook(title, author)
+			fmt.Println("\t\tBook added successfully")
 
 		case "remove":
 			bookID := GetInputInt(reader, "\tBook ID: ")
 			services.HandleRemoveBook(bookID)
+			fmt.Printf("\t\tBook with ID %v removed successfully\n", bookID)
 
 		case "borrow":
 			bookID := GetInputInt(reader, "\tBook ID: ")
 			memberID := GetInputInt(reader, "\tMember ID: ")
-			services.HandleBorrow(bookID, memberID)
+			err := services.HandleBorrow(bookID, memberID)
+			if err != nil {
+				fmt.Printf("\t\t**Error: %v", err.Error())
+				continue
+			}
+
+			fmt.Printf("\t\tMember with ID %v has borrowed book #%v\n", memberID, bookID)
 
 		case "return":
 			bookID := GetInputInt(reader, "\tBook ID: ")
 			memberID := GetInputInt(reader, "\tMember ID: ")
-			services.HandleReturn(bookID, memberID)
+			err := services.HandleReturn(bookID, memberID)
+			if err != nil {
+				fmt.Printf("\t\t**Error: %v", err.Error())
+				continue
+			}
+
+			fmt.Printf("\t\tBook with ID: %v returned successfully\n", bookID)
 
 		case "la":
-			services.HandleListAvailable()
+			availableBooks := services.HandleListAvailable()
+			LogBooks(availableBooks, "\tAvailable Books:")
+
 		case "lb":
-			services.HandleListBorrowed()
+			borrowedBooks := services.HandleListBorrowed()
+			LogBooks(borrowedBooks, "\tBorrowed Books:")
 
 		case "q":
 			active = false
--- 1_intro/task_3_library_management/services/library_service.go
+++ 1_intro/task_3_library_management/services/library_service.go
@@ -1,13 +1,32 @@
 package services
 
-func HandleAddBook(title string, author string) {}
+import "library_management/models"
 
-func HandleRemoveBook(bookID int) {}
+var nextInt = 0
+var LIB = models.Library{Books: make(map[int]models.Book), Members: make(map[int]models.Member)}
 
-func HandleBorrow(bookID int, memberID int) {}
+func HandleAddBook(title string, author string) {
+	newBook := models.Book{ID: nextInt, Title: title, Status: "available", Author: author}
+	nextInt++
+	LIB.AddBook(newBook)
+}
 
-func HandleReturn(bookID int, memberID int) {}
+func HandleRemoveBook(bookID int) {
+	LIB.RemoveBook(bookID)
+}
 
-func HandleListAvailable() {}
+func HandleBorrow(bookID int, memberID int) error {
+	return LIB.BorrowBook(bookID, memberID)
+}
 
-func HandleListBorrowed() {}
+func HandleReturn(bookID int, memberID int) error {
+	return LIB.ReturnBook(bookID, memberID)
+}
+
+func HandleListAvailable() []models.Book {
+	return LIB.ListAvailableBooks()
+}
+
+func HandleListBorrowed() []models.Book {
+	return LIB.ListBorrowedBooks()
+}
