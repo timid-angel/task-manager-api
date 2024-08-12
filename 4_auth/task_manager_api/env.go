50878aa9fef0394fe24cee8fe815077b43d5e2db
Nathan M.S <113636678+timid-angel@users.noreply.github.com>
2024-08-12 12:49:38 +0300

    Update env.go

--- 4_auth/task_manager_api/env.go
+++ 4_auth/task_manager_api/env.go
@@ -1,5 +1,5 @@
 package main
 
 // these variables will be made environment variables upon execution
-var DB_URL = "mongodb+srv://nathanmesfin:e4RUNeAznMgF32hV@cluster0.a87u2d2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+var DB_URL = ""
 var JWT_SECRET_TOKEN = "absikalsdjfl"
